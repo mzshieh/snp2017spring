7f63763c31b47de5db3f024c790e870cbf0a4aff
Min-Zheng Shieh <mzshieh@nctu.edu.tw>
2017-06-02 11:52:12 +0800

    Updates

--- lec11/lec11-1.py
+++ lec11/lec11-1.py
@@ -6,5 +6,12 @@ res = requests.get('https://mzshieh.github.io/snp2016/html/1.html')
 soup = BeautifulSoup(res.text, 'lxml')
 
 div = soup.find('div')
+print('===== whole tag ========================== begin =====')
 print(div)
+print('===== whole tag =========================== end ======')
+
+print('')
+
+print('===== only text ========================== begin =====')
 print(div.text)
+print('===== only text =========================== end ======')
--- lec11/lec11-2.py
+++ lec11/lec11-2.py
@@ -6,11 +6,20 @@ res = requests.get('https://mzshieh.github.io/snp2016/html/2.html')
 soup = BeautifulSoup(res.text, 'lxml')
 
 a = soup.find('a')
+print('===== whole tag ========================== begin =====')
 print(a)
+print('===== whole tag =========================== end ======')
+print('')
+print('===== only text ========================== begin =====')
 print(a.text)
+print('===== only text =========================== end ======')
 
+# access attributes like a dictionary
 print(a.get('href'))
 print(a['href'])
 
+# get return None
 print(a.get('xd'))
+
+# The following fails
 print(a['xd'])
